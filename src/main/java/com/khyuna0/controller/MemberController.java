0aeeee71c09186deaa12ce8949c58389a28e7f8a
khyuna0 <khohyuna0102@gmail.com>
2025-08-27 17:52:46 +0900

    에러메세지 리스트 출력

--- src/main/java/com/khyuna0/controller/MemberController.java
+++ src/main/java/com/khyuna0/controller/MemberController.java
@@ -1,10 +1,47 @@
 package com.khyuna0.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.khyuna.dto.MemberDto;
+import com.khyuna0.member.validation.MemberValidator;
 
 @Controller
 public class MemberController {
 
+	@RequestMapping(value = "/memberJoin")
+	public String membrtJoin() {
+		
+		return "memberJoin";
+	}
 	
-	
+	@RequestMapping(value = "/memberJoinOk")
+	public String membrtJoinOk(@ModelAttribute("memberDto") MemberDto memberDto, Model model , BindingResult result) {
+		
+		MemberValidator memberValidator = new MemberValidator();
+		memberValidator.validate(memberDto, result);
+		
+		if(result.hasErrors()) { // 오류 있는지 확인 (유효성 체크)
+			List<ObjectError> allErrors = result.getAllErrors(); // 모든 에러가 들어있음!
+			List<String> errorMsg = new ArrayList<String>();
+			
+			for (ObjectError error : allErrors) {
+				errorMsg.add(error.getDefaultMessage());// DefaltMessage = 에러 메세지
+			}
+			
+			model.addAttribute("signupError", "회원 가입에 실패하셨습니다.");
+			model.addAttribute("errorMsg", errorMsg);
+			
+			return "memberJoin";
+		}
+		
+		return "memberJoinOk";
+	}
 }
--- src/main/java/com/khyuna0/member/validation/MemberValidator.java
+++ src/main/java/com/khyuna0/member/validation/MemberValidator.java
@@ -22,19 +22,38 @@ public class MemberValidator implements Validator {
 		String confirmPassword = memberDto.getConfirmPassword();
 		String name = memberDto.getName();
 		String email = memberDto.getEmail();
-		int age = memberDto.getAge();
+		Integer age = memberDto.getAge();
 		
-		// 아이디가 공란인지 확인
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "id", "id.empty", "id는 필수 입력사항입니다."); // 에러, 필드명, 오류 코드, 메세지
+		// 공란인지 확인
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "id", "id.empty", "아이디는 필수 입력사항입니다."); // 에러, 필드명, 오류 코드, 메세지
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "pw.empty", "비밀번호는 필수 입력사항입니다.");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "confirmPassword", "cpw.empty", "비밀번호 확인은 필수 입력사항입니다.");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "name.empty", "이름은 필수 입력사항입니다.");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", "email.empty", "이메일은 필수 입력사항입니다.");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "age", "age.empty", "나이는 필수 입력사항입니다.");
 		// 아이디의 길이가 5자 이상인지 확인
+		
 		if (id != null && id.length() < 5) { // 조건이 참이면 에러 -> 아이디 길이가 5자 미만이면 에러
-			errors.rejectValue("id", "id.short", "id는 5자 이상이어야 합니다.");
+			errors.rejectValue("id", "id.short", "아이디는 5자 이상이어야 합니다.");
+		}
+		
+		// 비밀번호의 길이가 5자 이상인지 확인
+		if (password != null && password.length() < 5) { // 비밀번호 길이가 5자 미만이면 에러
+			errors.rejectValue("password", "pw.short", "비밀번호는 5자 이상이어야 합니다.");
 		}
 		
 		// 비밀번호 확인
 		if (password != null && !password.equals(confirmPassword) ) { // 비밀번호 란 공백, 비밀번호와 비밀번호 확인이 일치하지 않는 경우
 			errors.rejectValue("confirmPassword", "pw.mismatch", "비밀번호 확인이 일치하지 않습니다.");
 		}
+		
+		// 나이 확인, 18세 이상만 가입 가능
+		if ( age == null || age < 18 ) {
+			errors.rejectValue("age", "age.short", "18세 이상만 가입 가능합니다.");
+		}
+		
+		
+		
 	}
 
 }
